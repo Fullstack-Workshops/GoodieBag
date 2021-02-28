ff23520790d338d9bbaaebc174a25ea5d83f22b9
azrielgoldman <goldmanazriel@yahoo.com>
2021-02-28 17:19:25 -0500

    FE: Added single candy action to redux store

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,11 +1,17 @@
 import axios from 'axios';
+import { noExtendLeft } from 'sequelize/types/lib/operators';
 
 const initialState = {
-  candies: []
+  candies: [],
+  candy: {}
 }
 
 const GET_CANDIES = 'GET_CANDIES';
+const GET_CANDY = 'GET_CANDY';
+
 export const getCandies = candies => ({type: GET_CANDIES, candies});
+
+export const getCandy = candy => ({type: GET_CANDY, candy});
 
 export const fetchCandies = () => {
   return async dispatch => {
@@ -19,10 +25,24 @@ export const fetchCandies = () => {
   }
 }
 
+export const fetchCandy = id => {
+  return async dispatch => {
+    try {
+      const { data } = await axios.get(`/api/candies/${id}`);
+      dispatch(getCandy(data));
+    } catch (error) {
+      console.log('Unable to fetch candy');
+      console.error(error);
+    }
+  }
+}
+
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_CANDIES:
       return {...state, candies: action.candies}
+    case GET_CANDY:
+      return action.candy
     default:
       return state
   }
