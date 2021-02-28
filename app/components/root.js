abaf5ab2c862b63fcc84c3014f186410ce2b0f91
azrielgoldman <goldmanazriel@yahoo.com>
2021-02-28 15:53:37 -0500

    BE: Created Backend Route to serve up all candies

--- app/components/root.js
+++ app/components/root.js
@@ -1,16 +1,25 @@
 import React from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Candies from './Candies';
 
 const Root = () => {
   return (
+    <Router>
     <div>
       <nav>
         Goodie Bag
       </nav>
       <main>
-        <h1>Welcome to the Goodie Bag!</h1>
-        <p>What a nice home page for your goodies!</p>
+        <div id="main-container">
+          <h1>Welcome to the Goodie Bag!</h1>
+          <p>What a nice home page for your goodies!</p>
+          <Switch>
+            <Router path="/candies" component={Candies} />
+          </Switch>
+        </div>
       </main>
     </div>
+    </Router>
   )
 }
 
---
+++ server/api/candies.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const { Candy } = require('../db/index');
+
+router.get('/', async (req, res, next) => {
+  try {
+    let candies = await Candy.findAll();
+    res.json(candies);
+  } catch (error) {
+    next(error);
+  }
+})
+
+module.exports = router;
