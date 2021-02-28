266091964bddd07c9694539566aa5caea91de557
azrielgoldman <goldmanazriel@yahoo.com>
2021-02-28 16:08:40 -0500

    FE: Created action in redux store for fetching all candies

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,8 +1,28 @@
+import axios from 'axios';
 
-const initialState = {}
+const initialState = {
+  candies: []
+}
+
+const GET_CANDIES = 'GET_CANDIES';
+export const getCandies = candies => ({type: GET_CANDIES, candies});
+
+export const fetchCandies = () => {
+  return async dispatch => {
+    try {
+      const { data } = await axios.get('/api/candies');
+      dispatch(getCandies(data));
+    } catch (error) {
+      console.log('Unable to fetch candies');
+      console.error(error);
+    }
+  }
+}
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
+    case GET_CANDIES:
+      return {...state, candies: [...state, action.candies]}
     default:
       return state
   }
